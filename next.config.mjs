84e534de43f8fb5fcf548717f103988474b68590
Firas-Mahjoubi <mahjoubi.firas@esprit.tn>
2025-09-20 18:51:24 +0100

    added

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    // Configure allowed image qualities
+    qualities: [75, 85, 90, 95, 100],
+    // You can add other image configs here if needed
+  }
+};
 
 export default nextConfig;
